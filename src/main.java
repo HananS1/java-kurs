cdfd960a0b0400f2a24337945a31509c1a289395
Hanan Sajid <sajidhanan88@gmail.com>
2025-02-09 13:06:39 +0100

    Taschenrechner

--- src/main.java
+++ src/main.java
@@ -4,13 +4,13 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first number:");
+        System.out.println("Enter first Number:");
         double num1 = scanner.nextDouble();
 
         System.out.println("Enter an operator (+, -, *, /):");
         char operator = scanner.next().charAt(0);
 
-        System.out.println("Enter second number:");
+        System.out.println("Enter second Number:");
         double num2 = scanner.nextDouble();
 
         double result;
