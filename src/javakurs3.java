e95b60b8840580daf7c93b489eca6bc5d2f2ef4a
Hanan Sajid <sajidhanan88@gmail.com>
2025-02-09 12:37:01 +0100

    video 3

--- src/javakurs3.java
+++ src/javakurs3.java
@@ -2,7 +2,7 @@
     public static void main(String[] args) {
         int hund = 5;
         int number2 = 10;
-        String text = "ich habe ";
+        String text = "ich hab ";
         String text2 = " eier";
         System.out.println(text +  hund + text2);
     }
