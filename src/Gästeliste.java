307878a2f7d4d32781ff2b1217aab15dbab43bd0
Hanan Sajid <sajidhanan88@gmail.com>
2025-02-09 14:37:20 +0100

    Taschenrechner

---
+++ src/Gästeliste.java
@@ -0,0 +1,10 @@
+public class Gästeliste {
+    public static void main(String[] args) {
+       int gäste = 5;
+
+       gäste ++;
+       gäste -- ;
+
+         System.out.println("Es sind " + gäste + " Gäste eingeladen.");
+    }
+}
